9ca7ff42f070dbc20ed5a8c0aeef73bdf685b909
liangyuchen0014 <19376244@buaa.edu.cn>
2024-05-31 23:32:43 +0800

    feat:论元同步

--- src/components/myHome.vue
+++ src/components/myHome.vue
@@ -6,7 +6,6 @@
         <el-button size="small" type="primary">点击上传JSON</el-button>
       </el-upload>
       <el-button type="primary" @click="downloadJSON">导出JSON</el-button>
-
       <el-table :data="originalJson" @row-click="eventRevise">
         <el-table-column prop="clipID" label="片段ID" width="80">
         </el-table-column>
@@ -14,10 +13,8 @@
         </el-table-column>
         <el-table-column prop="isRevised" label="是否已校对" width="120">
         </el-table-column>
-
       </el-table>
     </el-aside>
-
     <el-container>
       <el-header>
         <el-select v-model="event_value" placeholder="请选择事件">
@@ -26,7 +23,18 @@
         </el-select>
       </el-header>
       <el-main>
+        <el-col :span="12">
+          <el-table :data="revisingEvent">
+            <el-table-column prop="argID" label="论元属性" width="140">
+            </el-table-column>
+            <el-table-column prop="value" label="值" width="280">
+            </el-table-column>
 
+          </el-table>
+        </el-col>
+        <el-col :span="12">
+          test
+        </el-col>
       </el-main>
     </el-container>
   </el-container>
@@ -43,6 +51,7 @@ export default {
       event_value: '',
       fileList: [],
       originalJson: [],
+      revisingEvent: []
     }
   },
   methods: {
@@ -58,7 +67,21 @@ export default {
       }
     },
     async eventRevise(row) {
-      console.log(row);
+      [
+        'arg0_ins',
+        'arg1_ins',
+        'arg2_ins',
+        'arg3_ins',
+        'arg4_ins',
+        'argM-LOC',
+        'argM-EXT',
+      ].forEach(argID => {
+        const value = argID.includes('ins') ? row['instance'][argID] : row[argID]; // 检查是否属于 instance
+        if (value != null && value != undefined) {
+          this.revisingEvent.push({ 'argID': argID, 'value': value });
+        }
+      });
+      console.log(this.revisingEvent);
     },
     downloadJSON() { }
   }
